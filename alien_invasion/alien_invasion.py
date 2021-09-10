192b68a8f9205240d7b4bcac90a5be0b694c84ae
Tyler Laquinta <tylerlaquinta@Tylers-Air.home>
2021-09-10 15:52:24 -0400

    updated screen & right ship movement

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -25,10 +25,8 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self.ship.update()
+            self._update_screen()            
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -38,6 +36,20 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
